dfb21c2b6ae6804857284042a86edabe64149ebc
koike.sh <newsn.4318@gmail.com>
2023-03-12 09:00:26 +0900

    Lesson15　課題微修正

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,10 +1,7 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
